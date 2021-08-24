8748a71a804ea3d5b56c575ac5d30fb9fbe4611a
baobaosieudebug <baobaosieudebug@gmail.com>
2021-08-24 23:49:41 +0700

    add cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,28 +2,25 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const route = require('./routes');
-// const db = require('./config/db')
-// const cookieParser = require('cookie-parser')
 const session = require('express-session');
 const MonggoDBSession = require('connect-mongodb-session')(session);
-
+const cors = require('cors')
 const sessionMiddleware = require('./middlewares/session.middleware');
 
 const store = new MonggoDBSession({
     uri:'mongodb+srv://team:vSEinrwcXSAwDoOi@cluster0.okfgn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     collection:'mySession'
 })
-//connect db
-// db.connect();
+
 app.use(express.json());
-// app.use(cookieParser('secret'))
+app.use(cors());
 app.use(session({
     secret:'secret',
     resave:false,
     saveUninitialized: false,
     store: store,
 }))
-// app.use(sessionMiddleware);
+
 route(app);
 
 app.listen(port, () => {
