0b8461b17cfb91b7b58b56a4db109ff24d77d032
baobaosieudebug <baobaosieudebug@gmail.com>
2021-08-26 10:39:48 +0700

    ref env file

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 8000;
-const route = require("./routes");
-const cors = require("cors");
-const db = require("./config/db");
 const dotenv = require('dotenv');
 dotenv.config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+const route = require("./routes");
+const cors = require("cors");
 
-db.connect();
 app.use(express.json());
 app.use(cors());
 route(app);
