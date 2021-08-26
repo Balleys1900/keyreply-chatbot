5f404036619b3556692eadfaa43bd132f15aa7b4
baobaosieudebug <baobaosieudebug@gmail.com>
2021-08-26 22:05:00 +0700

    ref(chatbotRouter): ref api store history

--- server/src/routes/chatbot.js
+++ server/src/routes/chatbot.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const chatbotController = require("../controllers/ChatbotController");
 
-router.post("/store",chatbotController.storeHistory);
 router.get("/history",chatbotController.getHistory);
-
+router.post("/history",chatbotController.storeHistory);
 router.post("/", chatbotController.navigateNode);
 
 
