881e16c838c6f663df59eb3c453f03f315bf49de
Balleys1900 <leminhtai325@gmail.com>
2021-08-26 21:38:00 +0700

    update API history

--- server/src/routes/chatbot.js
+++ server/src/routes/chatbot.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 const chatbotController = require("../controllers/ChatbotController");
 
-router.post("/store",chatbotController.storeHistory);
-router.get("/history",chatbotController.getHistory);
+router.post("/history", chatbotController.storeHistory);
+router.get("/history", chatbotController.getHistory);
 
 router.post("/", chatbotController.navigateNode);
-
-
-
 
 module.exports = router;
