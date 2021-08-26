38a7980c34a30abf377bb1e7d95e356e462c34dc
khaphan2806 <khaphan2806@gmail.com>
2021-08-26 13:10:17 +0700

    conflict

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT;
-const route = require("./routes");
-const cors = require("cors");
+
+const route = require('./routes');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
