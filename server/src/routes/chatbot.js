9c6a47876109d2fcb35d3191e55d2d54a330a168
baobaosieudebug <baobaosieudebug@gmail.com>
2021-08-26 18:43:56 +0700

    feat(chatBotRouter): add api store and get data history

--- server/src/routes/chatbot.js
+++ server/src/routes/chatbot.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const chatbotController = require("../controllers/ChatbotController");
 
+router.post("/store",chatbotController.storeHistory);
+router.get("/history",chatbotController.getHistory);
+
 router.post("/", chatbotController.navigateNode);
+
+
+
 
 module.exports = router;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,6 +5,7 @@ const { verifyToken, checkTokenToLogin } = require("../middlewares/auth");
 function route(app) {
   app.use("/api/v1/login", checkTokenToLogin, loginRouter);
   app.use("/api/v1/chatbot", verifyToken, chatbotRouter);
+  // app.use("api/v1/chatbot",verifyToken,chatbotRouter)
 }
 
 module.exports = route;
