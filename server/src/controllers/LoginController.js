5b16c8dd4e2509a4e8469be4acd824d0d1b8ae97
khaphan2806 <khaphan2806@gmail.com>
2021-08-30 23:47:13 +0700

    Update command API

--- server/src/controllers/LoginController.js
+++ server/src/controllers/LoginController.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 class LoginController {
   login(req, res) {
     const username = req.body.username;
+    const language = req.body.language;
     if (!username) {
       return res.status(400).json({
         status: 'fail',
@@ -11,21 +12,22 @@ class LoginController {
       });
     }
     const secretKey = process.env.ACCESS_TOKEN_SECRET;
-    const token = jwt.sign({ username }, secretKey, {
+    const token = jwt.sign({ username, language }, secretKey, {
       expiresIn: process.env.ACCESS_TOKEN_LIFE,
     });
 
     User.create({
       username: req.body.username,
-      access_token:token,
+      access_token: token,
       chatArr: [],
+      language: req.body.language,
     })
-        .then(() => {
-          return res.status(200).json(token);
-        })
-        .catch(err => {
-          return res.status(503).json({ msg: "Can't create user" });
-        });
+      .then(() => {
+        return res.status(200).json(token);
+      })
+      .catch(err => {
+        return res.status(503).json({ msg: "Can't create user" });
+      });
   }
 }
 
