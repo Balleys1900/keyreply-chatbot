90ebb8becb95e4606095ccd4b70da63b3be73a6b
khaphan2806 <khaphan2806@gmail.com>
2021-08-31 22:29:12 +0700

    Fix json file

--- client/src/components/ChatListItem.vue
+++ client/src/components/ChatListItem.vue
@@ -49,7 +49,7 @@ export default {
           }
         });
         productInfo.value = res.data.content;
-        console.log(productInfo.value);
+        console.log(res);
       } catch (error) {
         console.log(error);
       }
